c4f84cc1a245c337ec18277ed2d65ecd16ce89e9
Davidhct <davidhct1@gmail.com>
2021-07-11 17:04:13 +0300

    update js file

--- index.js
+++ index.js
@@ -10,23 +10,26 @@ function listenerSearchIcon() {
 }
 
 async function getWeatherRequest(inputVal) {
-  const key = "789e6aefa7f26f60027ce65cd1d704de";
-  const urlRequest = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${key}`;
+  try {
+    const apiKey = "789e6aefa7f26f60027ce65cd1d704de";
+    //const urlRequest = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${key}`;
 
-  fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${key}`
-  )
-    .then((res) => {
-      if (!res.ok) throw new Error(`Problem ${res.status}`);
-      return res.json();
-    })
-    .then((data) => {
-      displayWeather(data);
-    })
-    .catch((err) => console.error(err));
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&units=metric&appid=${apiKey}`
+    );
+
+    if (!res.ok) throw new Error(`Problem ${res.status}`);
+    const weatherData = await res.json();
+
+    renderWeather(weatherData);
+
+    return `Weather data: ${weatherData}`;
+  } catch (err) {
+    console.error(`${err.message} 🧨`);
+  }
 }
 
-function displayWeather(weatherData) {
+function renderWeather(weatherData) {
   const daysInWeek = [
     "Sunday",
     "Monday",
@@ -41,25 +44,35 @@ function displayWeather(weatherData) {
 
   let daysWeatherHtml = "";
   let days = weatherData.list.filter(
-    (item) => new Date(item.dt_txt).getHours() === 12
+    (item) => new Date(item.dt_txt).getHours() === 15
   );
 
   days.forEach((element) => {
     const day = new Date(element.dt_txt);
+    let date = modifyDate(element.dt_txt);
     daysWeatherHtml += `
       <div class="day-card">
       <div class="day">${daysInWeek[day.getDay()]}</div>
-      <div class="date">${element.dt_txt}</div>
+      <div class="date">${date}</div>
+      <div class="temp">${element.main.temp.toFixed(1)}9&#176C</div>
       <img src="http://openweathermap.org/img/wn/${
         element.weather[0].icon
       }@2x.png" class="icon"/>
       <div class="desc">${element.weather[0].description}</div>
-      <div class="temp">${element.main.temp}</div>
+      
       </div>
       `;
   });
+
   console.log(daysWeatherHtml);
 
   $(".cards").append(daysWeatherHtml);
   // $("main").append(cityHtml);
+}
+
+function modifyDate(dt) {
+  let [date, _] = dt.split(" ");
+  date = date.replaceAll("-", ".");
+
+  return date;
 }
